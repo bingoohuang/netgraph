e1c47e8e79461b84ffac2bbd785b46505ae14d5b
bingoo <bingoo.huang@gmail.com>
2021-05-16 19:20:33 +0800

    uniStreamsLock

--- pkg/httpstream/factory.go
+++ pkg/httpstream/factory.go
@@ -9,11 +9,12 @@ import (
 
 // Factory implements StreamFactory interface for tcpassembly.
 type Factory struct {
-	runningStream int32
-	wg            sync.WaitGroup
-	seq           uint
-	uniStreams    map[streamKey]*pair
-	eventChan     chan<- interface{}
+	runningStream  int32
+	wg             sync.WaitGroup
+	seq            uint
+	uniStreams     map[streamKey]*pair
+	uniStreamsLock sync.Mutex
+	eventChan      chan<- interface{}
 }
 
 // NewFactory create a NewFactory.
@@ -36,21 +37,28 @@ func (f *Factory) New(netFlow, tcpFlow gopacket.Flow) tcpassembly.Stream {
 	stream := newHTTPStream(key)
 
 	revkey := streamKey{net: netFlow.Reverse(), tcp: tcpFlow.Reverse()}
+	f.uniStreamsLock.Lock()
 	if p, ok := f.uniStreams[revkey]; ok {
 		delete(f.uniStreams, revkey)
+		f.uniStreamsLock.Unlock()
 		go p.run(stream)
 		return stream
 	}
 
 	p := newPair(f.seq, f.eventChan)
 	f.uniStreams[key] = p
+	f.uniStreamsLock.Unlock()
 	f.seq++
 	f.wg.Add(1)
 	go func() {
 		atomic.AddInt32(&f.runningStream, 1)
 		defer atomic.AddInt32(&f.runningStream, -1)
 		defer f.wg.Done()
-		defer delete(f.uniStreams, key)
+		defer func() {
+			f.uniStreamsLock.Lock()
+			delete(f.uniStreams, key)
+			f.uniStreamsLock.Unlock()
+		}()
 
 		p.run(stream)
 	}()
